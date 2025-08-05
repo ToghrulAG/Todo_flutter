1b791cc61c589bf592e143a1d638b68e0053fa59
Toghrul AG <tgrlagl@proton.me>
2025-08-05 04:07:18 +0400

    updated todo model

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+
 class TodoModel {
   int id;
   String title;
@@ -8,12 +10,30 @@ class TodoModel {
     required this.id,
     required this.title,
     this.isCompleted = false,
-    required this.priority
+    required this.priority,
   });
+
+  // DATA TO JSON 
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'isCompleted': isCompleted,
+      'priority': priority.index,
+    };
+  }
+
+  // JSON TO DATA
+  
+  factory TodoModel.fromJson(Map<String, dynamic> json) {
+    return TodoModel(
+      id: json['id'],
+      title: json['title'],
+      isCompleted: json['isCompleted'] ?? false,
+      priority: TodoPriority.values[json['priority']],
+    );
+  }
 }
 
-enum TodoPriority {
-  low,
-  medium,
-  high
-}
+enum TodoPriority { low, medium, high }
