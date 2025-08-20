e55861e8e87654f9981c7c3169c33061f8ae7ba3
Toghrul AG <tgrlagl@proton.me>
2025-08-20 14:44:46 +0400

    added todo method providers

---
+++ lib/providers/todo_provider.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todo_app/features/sharedPreferences.dart';
+import '../models/todo_model.dart';
+
+final todoProvider = StateNotifierProvider<TodoNotifier, List<TodoModel>>((
+  ref,
+) {
+  return TodoNotifier();
+});
+
+class TodoNotifier extends StateNotifier<List<TodoModel>> {
+  TodoNotifier() : super([]) {
+    _loadTodos();
+  }
+
+  Future<void> _loadTodos() async {
+    final loadedTodos = await loadTodos();
+    state = loadedTodos;
+  }
+
+  Future<void> addNewTodo(String title) async {
+    final newTodo = TodoModel(
+      id: state.length + 1,
+      title: title,
+      isCompleted: false,
+    );
+    state = [...state, newTodo];
+    await saveTodos(state);
+  }
+
+  
+}
